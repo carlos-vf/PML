b1bff3bb2674cfc648e94bd3f1e01791f6b51579
Fabyk <fabyk27@gmail.com>
2025-06-12 00:18:57 +0200

    pipeline for grid added

---
+++ scripts/run_grid.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import argparse
+import yaml
+import os
+import numpy as np
+import sys
+# Import your existing experiment functions (you'll create this module next)
+from scripts.pipeline_grid import run_pipeline
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Run experiment from YAML config")
+    parser.add_argument("--config", type=str, required=True, help="Path to YAML config file")
+    args = parser.parse_args()
+
+    with open(args.config, "r") as f:
+        config = yaml.safe_load(f)
+
+    # Run pipeline for a single dataset config
+    run_pipeline(config)
+
+
+if __name__ == "__main__":
+    main()
